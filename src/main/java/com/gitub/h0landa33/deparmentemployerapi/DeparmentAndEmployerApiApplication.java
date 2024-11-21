7708e73c1f7502a766279e204da147fea7e54e1d
Samuel11238902 <samuel112928@gmail.com>
2024-11-21 12:50:37 -0300

    Subindo o projeto local para remoto

---
+++ src/main/java/com/gitub/h0landa33/deparmentemployerapi/DeparmentAndEmployerApiApplication.java
@@ -0,0 +1,13 @@
+package com.gitub.h0landa33.deparmentemployerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeparmentAndEmployerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeparmentAndEmployerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitub/h0landa33/deparmentemployerapi/DeparmentAndEmployerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitub.h0landa33.deparmentemployerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeparmentAndEmployerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
